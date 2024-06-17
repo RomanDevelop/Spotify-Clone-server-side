2f8527b70057e7908b3be47475931e64e405d04a
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2024-06-17 20:59:11 +0200

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
-def test(): return 'hello world'
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    password: str
+
+@app.post('/signup')
+def signup_user():
+    # 
+    pass
