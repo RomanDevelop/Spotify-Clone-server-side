5f7cf95768cf8e0d6ce2c050fd9ac31287e10bc6
Passionate Flutter developer <129858140+RomanDevelop@users.noreply.github.com>
2024-07-18 09:16:27 +0200

    sign up funct

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ class UserCreate(BaseModel):
     password: str
 
 @app.post('/signup')
-def signup_user():
-    # 
+def signup_user(user: UserCreate):
+    # extract the data thats coming from req
+    print(user.name)
+    print(user.email)
+    print(user.password)
+    # check if the user already exists in db
+    # add the user to the db
     pass
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/anymacstore/Flutter development/musicapp/server/venv"
+set -gx VIRTUAL_ENV "/Users/anymacstore/Flutter development/Spotify-Clone-server-side/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
